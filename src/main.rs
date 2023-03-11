bc066fbb37a285105bdbbf64382b6e177f967e12
Iker Hernandez <iker@grabit.ai>
2023-03-11 18:49:58 +0100

    ADDED: NRZ encoder & changed to bit_vec

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use csv::Writer;
-use env_logger;
-// Basic logger
+use bit_vec::BitVec;
+use csv::Writer; // Input/Output reader // CSV file writer
+use env_logger; // Basic logger
 use std::fs::OpenOptions; //Filesystem
-use std::io; // Input/Output reader // CSV file writer
+use std::io;
 
 #[macro_use]
 extern crate log;
@@ -24,15 +24,18 @@ fn main() {
     info!("simb_tem created: {}", simb_tem);
     let modulation_tem: f64 = x / (f64::from(modulation_freq));
     info!("modulation_tem created: {}", modulation_tem);
-
     let sampling_tem: f64 = x / (f64::from(sampling_freq));
     info!("sampling_tem created: {}", sampling_tem);
 
     // Even bit demoultiplex input data
     let (odd_bits, even_bits) = demultiplexor_even(bit_data);
 
+    // Bits to NRZ signal
+    let odd_sig = nrz_encoder(odd_bits.clone(), f64::from(1));
+    let odd_sig = nrz_encoder(even_bits.clone(), f64::from(1));
+
     // Write value in a CSV
-    save_in_csv("test.csv", &bit_array_to_string(&odd_bits));
+    save_in_csv("test.csv", odd_bits);
 }
 
 ///  Returns the number that the user entered via the console line
@@ -76,7 +79,7 @@ fn read_number(parameter: &str) -> u32 {
 /// let number = read_number("Test")
 /// '''
 
-fn read_even_bit_array_from_console(parameter: &str) -> Vec<bool> {
+fn read_even_bit_array_from_console(parameter: &str) -> BitVec {
     // Keeps asking the user for a even number of bits
     loop {
         // Shows the name of the parameter that the user has to enter
@@ -90,12 +93,22 @@ fn read_even_bit_array_from_console(parameter: &str) -> Vec<bool> {
             .expect("Failed to read line");
         info!("{} parameter entered:{}", parameter, input);
 
+        let mut bits: BitVec = BitVec::new();
+
+        for c in input.chars() {
+            if c == '1' {
+                bits.push(true);
+            } else if c == '0' {
+                bits.push(false);
+            }
+        }
+        info!("created bit array:{:?}", bits);
+
         //Format the input data to an array of bits
-        let bits: Vec<bool> = input.trim().chars().map(|c| c == '1').collect();
 
         // Check if the entered bit amount is even
         if bits.len() % 2 == 0 {
-            info!("{} parameter returned:{}", parameter, input);
+            info!("{} parameter returned:{:?}", parameter, bits);
             return bits;
         } else {
             error!("Entered bit array doesn't have an even number of bits");
@@ -109,7 +122,7 @@ fn read_even_bit_array_from_console(parameter: &str) -> Vec<bool> {
 /// # Arguments
 ///  -`file_name`(&str): Name of the file to be used
 ///
-fn save_in_csv(file_name: &str, values: &str) {
+fn save_in_csv(file_name: &str, values: BitVec) {
     //TODO: Add file path checker
     //Creates a new file if it doesn't already exist
     let file = OpenOptions::new()
@@ -121,8 +134,12 @@ fn save_in_csv(file_name: &str, values: &str) {
     // Create a CSV writer from the file
     let mut wtr = Writer::from_writer(file);
 
+    let values_string: String = values
+        .iter()
+        .map(|bit| if bit { '1' } else { '0' })
+        .collect();
     // Write some records to the CSV file
-    wtr.write_record(&["name", values]).unwrap();
+    wtr.write_record(&["name", &values_string]).unwrap();
 
     // Flush the writer to ensure everything is written
     wtr.flush().unwrap();
@@ -130,39 +147,43 @@ fn save_in_csv(file_name: &str, values: &str) {
 
 /// Even bit demultiplexor for bit array
 ///
-fn demultiplexor_even(data: Vec<bool>) -> (Vec<bool>, Vec<bool>) {
-    let mut odd_data = vec![];
-    let mut even_data = vec![];
+fn demultiplexor_even(data: BitVec) -> (BitVec, BitVec) {
+    let mut odd_data = BitVec::new();
+    let mut even_data = BitVec::new();
 
     // Go through every single bit in the input array
     for (i, bit) in data.iter().enumerate() {
         // Check if the position of the bit is even or odd, i starts at 0
         if i % 2 == 0 {
             // Bit position is odd
-            odd_data.push(*bit);
+            odd_data.push(bit);
         } else {
             // Bit position is even
-            even_data.push(*bit);
+            even_data.push(bit);
         }
     }
     info!(
-        "Input: {} \n Odd bit array:  {} \n  Even bit array: {} ",
-        bit_array_to_string(&data),
-        bit_array_to_string(&odd_data),
-        bit_array_to_string(&even_data)
+        "Input:{:?}  Odd bit array:{:?} Even bit array:{:?} ",
+        data, odd_data, even_data
     );
 
     (odd_data, even_data)
 }
 
-fn bit_array_to_string(bits: &[bool]) -> String {
-    let mut s = String::new();
-    for bit in bits {
-        if *bit {
-            s.push('1');
+/// NRZ Encoder
+/// Transforms 1 to sqrt(Eb) and 0 to -sqrt(Eb)
+
+fn nrz_encoder(data: BitVec, eb: f64) -> Vec<f64> {
+    let eb_sqrt = f64::sqrt(eb);
+    let mut result = Vec::new();
+
+    for (_i, bit) in data.iter().enumerate() {
+        if bit {
+            result.push(eb_sqrt);
         } else {
-            s.push('0');
+            result.push(-eb_sqrt);
         }
     }
-    s
+    info!("NRZ Encoder - Input: {:?} - Output: {:?}", data, result);
+    result
 }
